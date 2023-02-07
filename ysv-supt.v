9ffca96b26602c134d33d2302a4d0d269e698a96
Thomas J. Moore <darktjm@gmail.com>
2023-02-07 06:11:57 -0500

    I made changes to ysv-supt.v before last commit, but it wasn't checked in.  No idea why.

--- ysv-supt.v
+++ ysv-supt.v
@@ -1,10 +1,19 @@
 // Yosys doesn't support non-memory arrays of vectors.
+// The following macros make access using plain vectors slightly cleaner.
+// Probably a cleaner approach would be to have `va(name,sz,n) define a macro
+// `<name>_size that is used in the other macros to get the size, rather than
+// needing manual size repetition.  For now, this is good enough, though.
+// Also, I'm not sure all preprocessors are up to the task.
+
 // Vector array:
 //   wire [N-1:0] array [M-1:0] -> wire [`va(N,M)] array
 `define va(sz,n)     (sz)*(n)-1:0
-// Elelemnt select:
+// Array slice select:
+//   array[h:l] -> array[M*h:M*l]
+`define vas(sz,h,l) ((sz)*((h)+1)-1):(sz)*(l)
+// Array elelemnt select:
 //   array[i] -> array[`vai(M,i)]
-`define vai(sz,e)    (sz)*((e)+1)-1:(sz)*(e)
+`define vai(sz,e)    `vas(sz,e,e)
 // Element slice select:
 //   array[i][h:l] -> array[`vais(M,i,h,l)]
 `define vais(sz,e,h,l) (sz)*((e)+1)-((sz)-h):(sz)*(e)+(l)
